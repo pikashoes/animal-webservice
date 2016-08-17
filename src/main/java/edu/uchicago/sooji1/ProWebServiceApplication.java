4b79b6e820b985bed597eb3d4840f269b193f54e
Susie Yi <sooji1@cs.uchicago.edu>
2016-08-17 16:32:18 -0500

    init commit

---
+++ src/main/java/edu/uchicago/sooji1/ProWebServiceApplication.java
@@ -0,0 +1,12 @@
+package edu.uchicago.sooji1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProWebServiceApplication.class, args);
+	}
+}
